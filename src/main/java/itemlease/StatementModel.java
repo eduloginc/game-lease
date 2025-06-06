051ab5f368b38ab5f17639c363393f2469026d3e
Nicolas Peron <nperon@gmail.com>
2025-06-06 21:30:58 +0200

    cleaning: move logics into StatementModel

--- src/main/java/itemlease/StatementModel.java
+++ src/main/java/itemlease/StatementModel.java
@@ -24,4 +24,33 @@ public class StatementModel {
     public void setNbLoyaltyPoints(int nbLoyaltyPoints) {
         this.nbLoyaltyPoints = nbLoyaltyPoints;
     }
+
+    static double leaseAmount(Lease lease) {
+        double amount = 0;
+        switch (lease.getGame().getPriceCode()) {
+            case LeaseItem.REGULAR:
+                amount += 2;
+                if (lease.getDaysLeased() > 2)
+                    amount += (lease.getDaysLeased() - 2) * 1.5;
+                break;
+            case LeaseItem.NEWLY_RELEASED:
+                amount += lease.getDaysLeased() * 3;
+                break;
+            case LeaseItem.CHILDREN:
+                amount += 1.5;
+                if (lease.getDaysLeased() > 3)
+                    amount += (lease.getDaysLeased() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public double addLeaseAndReturnAmountAdded(Lease each) {
+        double amount = leaseAmount(each);
+        this.setNbLoyaltyPoints(this.getNbLoyaltyPoints() + 1);
+        if ((each.getGame().getPriceCode() == LeaseItem.NEWLY_RELEASED) && each.getDaysLeased() > 1)
+            this.setNbLoyaltyPoints(this.getNbLoyaltyPoints() + 1);
+        this.setTotalAmount(this.getTotalAmount() + amount);
+        return amount;
+    }
 }
--- src/main/java/itemlease/TextStatementFormatter.java
+++ src/main/java/itemlease/TextStatementFormatter.java
@@ -1,7 +1,7 @@
 package itemlease;
 
 public class TextStatementFormatter implements StatementFormatter {
-    
+
     @Override
     public String format(Customer customer) {
 
@@ -10,27 +10,8 @@ public class TextStatementFormatter implements StatementFormatter {
         StringBuilder result = new StringBuilder("Games leased by " + customer.getName() + "\n");
 
         for (Lease each : customer.getLeases()) {
-            double thisAmount = 0;
-            switch (each.getGame().getPriceCode()) {
-                case LeaseItem.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysLeased() > 2)
-                        thisAmount += (each.getDaysLeased() - 2) * 1.5;
-                    break;
-                case LeaseItem.NEWLY_RELEASED:
-                    thisAmount += each.getDaysLeased() * 3;
-                    break;
-                case LeaseItem.CHILDREN:
-                    thisAmount += 1.5;
-                    if (each.getDaysLeased() > 3)
-                        thisAmount += (each.getDaysLeased() - 3) * 1.5;
-                    break;
-            }
-            statementModel.setNbLoyaltyPoints(statementModel.getNbLoyaltyPoints() + 1);
-            if ((each.getGame().getPriceCode() == LeaseItem.NEWLY_RELEASED) && each.getDaysLeased() > 1)
-                statementModel.setNbLoyaltyPoints(statementModel.getNbLoyaltyPoints() + 1);
-            result.append("\t").append(each.getGame().getTitle()).append("\t").append(thisAmount).append("\n");
-            statementModel.setTotalAmount(statementModel.getTotalAmount() + thisAmount);
+            double amount = statementModel.addLeaseAndReturnAmountAdded(each);
+            result.append("\t").append(each.getGame().getTitle()).append("\t").append(amount).append("\n");
         }
         result.append("Amount is ").append(statementModel.getTotalAmount()).append("\n");
         result.append("You earned ").append(statementModel.getNbLoyaltyPoints()).append(" loyalty points");
